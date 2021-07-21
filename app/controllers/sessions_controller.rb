b47d3d3170eabae80ce7f3fa701356daee7f31e5
Dakota States <dakota.states@gmail.com>
2021-07-21 13:05:11 -0700

    Registrations Controller and current user concern, loggedin and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User
             .find_by(email: params["user"]["email"])
@@ -7,7 +9,7 @@ class SessionsController < ApplicationController
     if user
       session[:user_id] = user.id
       render json: {
-        status: "created", 
+        status: "created",
         logged_in: true,
         user: user
       }
@@ -16,4 +18,23 @@ class SessionsController < ApplicationController
       render json: {status: 401 }
     end
   end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json:{ status: 200, logged_out: true }
+  end
+
 end
